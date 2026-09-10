a87c544cd251f1acad41a1e8f5ef35d39e469061
SenhasFestas Dev <dev@senhasfestas.com>
2026-09-10 14:30:49 +0100

    fix: backend voltava a falhar ao arrancar (CashClosureEntity em falta no EventModule)

--- backend/src/modules/event/event.module.ts
+++ backend/src/modules/event/event.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventController } from './event.controller';
 import { EventService } from './event.service';
-import { EventEntity, EventUserEntity, OrderEntity, BalanceEntity, ProductEntity, BalanceMovementEntity, CategoryEntity, StationEntity } from '../../entities';
+import { EventEntity, EventUserEntity, OrderEntity, BalanceEntity, ProductEntity, BalanceMovementEntity, CategoryEntity, StationEntity, CashClosureEntity } from '../../entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EventEntity, EventUserEntity, OrderEntity, BalanceEntity, ProductEntity, BalanceMovementEntity, CategoryEntity, StationEntity])],
+  imports: [TypeOrmModule.forFeature([EventEntity, EventUserEntity, OrderEntity, BalanceEntity, ProductEntity, BalanceMovementEntity, CategoryEntity, StationEntity, CashClosureEntity])],
   controllers: [EventController],
   providers: [EventService],
   exports: [EventService],
